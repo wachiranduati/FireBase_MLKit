a6f4f2be7abcf695c9aaf9bca60f445319bef68b
Wachira Nduati <wachiranduati@gmail.com>
2021-03-11 22:27:10 +0300

    implement label image screen

--- app/src/main/java/tiiva/nduati/firebasemlkit/MainActivity.kt
+++ app/src/main/java/tiiva/nduati/firebasemlkit/MainActivity.kt
@@ -2,10 +2,32 @@ package tiiva.nduati.firebasemlkit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    lateinit var readBtn : Button
+    lateinit var imageLnk : EditText
+    lateinit var mlImgVw : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        readBtn = findViewById(R.id.readImageButton)
+        imageLnk = findViewById(R.id.imagelinkEditText)
+        mlImgVw = findViewById(R.id.mlImageView)
+
+        readBtn.setOnClickListener {
+            if(imageLnk.text.toString().isNotEmpty()){
+                Glide.with(this).load(imageLnk.text.toString()).into(mlImgVw)
+                readImageText()
+            }
+        }
+    }
+
+    private fun readImageText() {
+        TODO("Not yet implemented")
     }
 }
