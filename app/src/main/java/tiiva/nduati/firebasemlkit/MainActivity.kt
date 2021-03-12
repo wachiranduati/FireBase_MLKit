355180327173e7d3135e3d82386561f0a6a2a544
Wachira Nduati <wachiranduati@gmail.com>
2021-03-12 22:41:37 +0300

    text recognition working

--- app/src/main/java/tiiva/nduati/firebasemlkit/MainActivity.kt
+++ app/src/main/java/tiiva/nduati/firebasemlkit/MainActivity.kt
@@ -1,11 +1,19 @@
 package tiiva.nduati.firebasemlkit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var readBtn : Button
@@ -21,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         readBtn.setOnClickListener {
             if(imageLnk.text.toString().isNotEmpty()){
+//                Glide.with(this).load(R.drawable.france).into(mlImgVw)
                 Glide.with(this).load(imageLnk.text.toString()).into(mlImgVw)
                 readImageText()
             }
@@ -28,6 +37,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun readImageText() {
-        TODO("Not yet implemented")
+        Log.d(Companion.TAG, "readImageText: method has been called")
+        val bitmp = BitmapFactory.decodeResource(resources, R.drawable.france)
+        val image = InputImage.fromBitmap(bitmp, 0)
+        val recognizer = TextRecognition.getClient()
+
+        val result = recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                Toast.makeText(this, "${visionText.text}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                Log.d(TAG, "readImageText: text recognition failed")
+            }
+
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
